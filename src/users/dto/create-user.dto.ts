3678732ea73226a84f9dd3481d3ac8499ffccc58
aka <akatsukisatoshi@gmail.com>
2022-06-22 21:47:47 +0900

    ユーザー新規作成時にパラメーターのバリデーションをする

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({
+    message: 'nameは文字列を指定してください。',
+  })
+  readonly name: string;
+
+  @IsEmail(undefined, {
+    message: 'emailはEメールの形式になった文字列である必要があります。',
+  })
+  readonly email: string;
+
+  @IsString({
+    message: 'passwordは文字列を指定してください。',
+  })
+  @MinLength(6, {
+    message: 'passwordは6文字以上である必要があります。',
+  })
+  readonly password: string;
+}
